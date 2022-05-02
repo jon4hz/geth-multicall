6042f635e6fd80289145107bd8a19e23b6f07645
jon4hz <me@jon4hz.io>
2022-05-02 23:29:20 +0200

    add more contract addresses

--- multicall/options.go
+++ multicall/options.go
@@ -10,10 +10,13 @@ type Config struct {
 }
 
 const (
-	// MainnetMulticall : Multicall contract address on mainnet
-	MainnetAddress = "0x5eb3fa2dfecdde21c950813c665e9364fa609bd2"
-	// RopstenMulticall : Multicall contract address on Ropsten
-	RopstenAddress = "0xf3ad7e31b052ff96566eedd218a823430e74b406"
+	MainnetAddress    = "0x5eb3fa2dfecdde21c950813c665e9364fa609bd2"
+	RopstenAddress    = "0xf3ad7e31b052ff96566eedd218a823430e74b406"
+	BSCAddress        = "0x6Cf63cC81660Dd174A49e0C61A1f916456Ee1471"
+	BSCTestnetAddress = "0xD3c6D8dAa57dfD38609047447cccDEF7Db6631b5"
+	PolygonAddress    = "0x8a233a018a2e123c0D96435CF99c8e65648b429F"
+	FantomAddress     = "0x08AB4aa09F43cF2D45046870170dd75AE6FBa306"
+	CronosAddress     = "0x845C4753954c347175B4179B2D5B18DE1629f94F"
 )
 
 func WithContractAddress(address string) Option {
